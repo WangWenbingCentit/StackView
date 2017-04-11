b60ee74ec74f58c69dfcb0034699ba009e10eb73
wang_wb@centit.com <wang_wb@centit.com>
2017-04-11 16:22:47 +0800

    Initial Commit

---
+++ StackView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackView
+//
+//  Created by wangwenbing on 2017/4/11.
+//  Copyright © 2017年 Centit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
